716ca45c56afa102919a111f1c55381133be3e1f
Thiago Augusto <thiago.digitalfront@gmail.com>
2021-03-18 06:36:12 -0300

    auth flow

--- src/app/pages/guest/forgot-page/forgot-page.component.ts
+++ src/app/pages/guest/forgot-page/forgot-page.component.ts
@@ -8,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+  public messages = null;
+  public disableBtn = false;
+
+  infoDanger(msg){
+    UIkit.notification({
+      message: `<div class="uk-text-center">
+      ${ msg['email'] ? '<span>'+msg['email']+'</span><br>' : '' }
+      </div>`,
+      status: 'danger',
+      pos: 'top-center',
+      timeout: 5000
+    });
+}
+infoSuccess(msg){
+  UIkit.notification({
+    message: `<div class="uk-text-center"><span>${msg}</span></div>`,
+    status: 'success',
+    pos: 'top-center',
+    timeout: 5000
+  });
+}
+  onSubmit(body){
+    this.disableBtn = true;
+    return this.http.makeForgot(body).subscribe(
+      success => {
+        this.infoSuccess('Por favor, verifique seu email')
+        console.log(success);
+        this.disableBtn = false;
+        return this.router.navigate(['/login'])
+      },
+      error => {
+        this.infoDanger(error.error),
+        this.disableBtn = false;
+      }
+      
+    );
   }
 
 }
